9987ae16b8709b6b907a9406cadd2cc99dceb1c9
ananiah <ananiah_bao@qq.com>
2019-09-11 17:10:02 +0800

    9.11

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header id="header">
+        <h1>喵喵影院</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name : 'Header'
+    }
+</script>
+
+<style scoped>
+    #header{
+        width: 100%;
+        height: 50px;
+        color:#fff;
+        background: #ff0000;
+        border-bottom: 1px solid #ff0000;
+        position: relative;
+    }
+    #header h1{
+        font-size:18px;
+        text-align: center;
+        line-height: 50px;
+        font-weight: normal;
+    }
+    #header i{
+        position: absolute;
+        left: 5px;
+        top: 50%;
+        margin-top: -13px;
+        font-size: 26px;
+    }
+</style>
---
+++ src/views/Movie/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="main">
+        <Header />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header/Header.vue';
+import Footer from '@/components/Footer/Footer.vue';
+
+    export default {
+        name: 'Movie',
+        components : {
+            Header,
+            Footer
+        }
+    }
+</script>
+
+<style>
+
+</style>
